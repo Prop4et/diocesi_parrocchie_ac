e58115282fd49d9f35e944d9fcff1691499ff94f
Prop4et <francostanco97@gmail.com>
2024-09-08 18:29:48 +0200

    feat: add show checkbox

--- script/init_script.js
+++ script/init_script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([43.16296, 13.72274], 13);
+var map = L.map('map').setView([43.16296, 13.72274], 11);
 // var map = L.map('map').setView([-104.05, 48.99], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
---
+++ script/parrocchie_script.js
@@ -0,0 +1,9 @@
+
+
+// document.getElementById('toggleLayer1').addEventListener('change', function() {
+//     if (this.checked) {
+//         map.addLayer(parrocchie); // Add the first layer if checked
+//     } else {
+//         map.removeLayer(parrocchie); // Remove the first layer if unchecked
+//     }
+// });
