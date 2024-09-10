4440eefe0257b06f9051fbc9fe5da4539e35efe8
Francesco Biancucci <fbiancucci@sigmaspa.com>
2024-09-10 13:41:51 +0200

    fix: add data

--- script/coordinamenti_script.js
+++ script/coordinamenti_script.js
@@ -1,17 +1,164 @@
 
 
-var parrocchieCoord =[{
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [43.2627,13.7203]
+var parrocchieCoord =[
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [41.916700,13.7203]
+        },
+        "properties": {
+            "name": "SS. Redentore",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: SS. Redentore <p><b>Comune</b>: Casette d'Ete <p><b>Coordinatore</b>: Adriana Ciferri"
+        }
     },
-    "properties": {
-      "name": "SS Redentore",
-      "tipologia": "Coordinamento",
-      "popupContent":"<p><b>Parrocchia</b>: SS Redentore <p><b>Comune</b>: Casette d'Ete <p><b>Presidente</b>: Enrico Rossi"
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.265560, 13.713810]
+        },
+        "properties": {
+            "name": "SS. Angeli Custodi",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: SS. Angeli Custodi <p><b>Comune</b>: Cascinare <p><b>Coordinatore</b>: Alessia Barbi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.110760, 13.566400]
+        },
+        "properties": {
+            "name": "S. Paolo Apostolo",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Paolo Apostolo <p><b>Comune</b>: Piane di Montegiorgio <p><b>Coordinatore</b>: Arianna Vecchi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.3115772,13.7276278]
+        },
+        "properties": {
+            "name": "Cristo Re",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: Cristo Re <p><b>Comune</b>: Civitanova Marche <p><b>Coordinatore</b>: Elena Zanetti"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.229237, 13.685735]
+        },
+        "properties": {
+            "name": "Sant'Elpidio Abate",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: Sant'Elpidio Abate <p><b>Comune</b>: S. Elpidio a Mare <p><b>Coordinatore</b>: Elisa Meconi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [42.9781048, 13.352206]
+        },
+        "properties": {
+            "name": "SS. Ilario e Donato",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: SS. Ilario e Donato <p><b>Comune</b>: Amandola <p><b>Coordinatore</b>: Emanuele Cocci"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.366359, 13.6232259]
+        },
+        "properties": {
+            "name": "SS. Stefano e Giacomo",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: SS. Stefano e Giacomo <p><b>Comune</b>: Potenza Picena <p><b>Coordinatore</b>: Giovanna Pesareis"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.2366026, 13.5953938] 
+        },
+        "properties": {
+            "name": "S. Stefano Protomartire",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Stefano Protomartire <p><b>Comune</b>: Monte S. Giusto <p><b>Coordinatore</b>: Keti Silenzi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.3161606, 13.6808858] 
+        },
+        "properties": {
+            "name": "S. Paolo Apostolo",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Paolo Apostolo <p><b>Comune</b>: Civitanova Alta <p><b>Coordinatore</b>: Luca Ferrari"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.1801957, 13.7931002] 
+        },
+        "properties": {
+            "name": "S. Giorgio Martire",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Giorgio Martire <p><b>Comune</b>: Porto S. Giorgio <p><b>Coordinatore</b>: Maria Assunta Matricardi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.1602981, 13.7193074] 
+        },
+        "properties": {
+            "name": "S. Domenico",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Domenico <p><b>Comune</b>: Fermo <p><b>Coordinatore</b>: Maria Assunta Ciccaleni"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.166364, 13.4163228]
+        },
+        "properties": {
+            "name": "S. Maria",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Maria <p><b>Comune</b>: Loro Piceno <p><b>Coordinatore</b>: Sandro Ciabocco"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.2570596, 13.7413156] 
+        },
+        "properties": {
+            "name": "S. Maria Addolorata",
+            "tipologia": "Coordinamento",
+            "popupContent":"<p><b>Parrocchia</b>: S. Maria Addolorata <p><b>Comune</b>: Porto S. Elpidio <p><b>Coordinatore</b>: Sarah Luciani"
+        }
     }
-}]
+]
+
 
 var geojsonMarkerOptions = {
     radius: 8,
@@ -24,7 +171,7 @@ var geojsonMarkerOptions = {
 
 var parrocchieCoordLayer = L.geoJSON(parrocchieCoord, {
     pointToLayer: function (feature, latlng) {
-        return L.circleMarker(L.latLng(43.2627,13.7203), geojsonMarkerOptions);
+        return L.circleMarker(L.latLng(feature.geometry.coordinates[0], feature.geometry.coordinates[1]), geojsonMarkerOptions);
     },
     onEachFeature: onEachFeature
 }).addTo(map);
--- script/presidenze_script.js
+++ script/presidenze_script.js
@@ -1,16 +1,137 @@
-var parrocchiePres =[{
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [43.16296, 13.72274]
+var parrocchiePres =[
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.1561235, 13.7350437]
+        },
+        "properties": {
+            "name": "Santa Caterina d'Alessandria",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: Santa Caterina d'Alessandria<p><b>Comune</b>: Fermo <p><b>Presidente</b>: Enrico Rossi"
+        }
     },
-    "properties": {
-      "name": "Santa Caterina",
-      "tipologia": "Presidenza",
-      "popupContent":"<p><b>Parrocchia</b>: Santa Caterina <p><b>Comune</b>: Fermo <p><b>Presidente</b>: Enrico Rossi"
-      
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.359667, 13.695094]
+        },
+        "properties": {
+            "name": "Sant'Anna",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: Sant'Anna' <p><b>Comune</b>: Porto Potenza Picena <p><b>Presidente</b>: Arianna Galassi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.313944, 13.582937]
+        },
+        "properties": {
+            "name": "S. Bartolomeo Apostolo",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: S. Bartolomeo Apostolo <p><b>Comune</b>: Morrovalle <p><b>Presidente</b>: Camilla Marchionni"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.111757, 13.603666]
+        },
+        "properties": {
+            "name": "San Giovanni Battista",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: San Giovanni Battista <p><b>Comune</b>: Grottazzolina <p><b>Presidente</b>: Filippo Scibè"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.268487, 13.751238]
+        },
+        "properties": {
+            "name": "S. Pio X",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: S. Pio X <p><b>Comune</b>: Porto S. Elpidio <p><b>Presidente</b>: Francesca Carassai"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.128876, 13.626707]
+        },
+        "properties": {
+            "name": "Sacro Cuore di Gesù",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: Sacro Cuore di Gesù <p><b>Comune</b>: Ponzano di Fermo <p><b>Presidente</b>: Joele Tosoni"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.249033, 13.511822]
+        },
+        "properties": {
+            "name": "SS. Pietro, Paolo e Donato",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: SS. Pietro, Paolo e Donato <p><b>Comune</b>: Corridonia <p><b>Presidente</b>: Mariangela Scarficcia"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [48.137264, 11.581357]
+        },
+        "properties": {
+            "name": "Gruppo Estero",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: Gruppo Estero <p><b>Comune</b>: Monaco Di Baviera <p><b>Presidente</b>: Alberto Damiani"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.206154, 13.670812]
+        },
+        "properties": {
+            "name": "S. Michele Arcangelo",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: S. Michele Arcangelo <p><b>Comune</b>: Monte Urano <p><b>Presidente</b>: Noemi Iacoponi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.132454, 13.536757]
+        },
+        "properties": {
+            "name": "SS. Giovanni B. e Nicolò",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: SS. Giovanni B. e Nicolò <p><b>Comune</b>: Montegiorgio <p><b>Presidente</b>: Samuele Tempestilli"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.227681, 13.626882]
+        },
+        "properties": {
+            "name": "UP San Serafino",
+            "tipologia": "Presidenza",
+            "popupContent":"<p><b>Parrocchia</b>: UP San Serafino <p><b>Comune</b>: Montegranaro <p><b>Presidente</b>: Tonia Lattanzi"
+        }
     }
-}]
+]
 
 var geojsonMarkerOptions = {
     radius: 8,
@@ -30,7 +151,7 @@ function onEachFeature(feature, layer) {
 
 var parrocchiePresLayer = L.geoJSON(parrocchiePres, {
     pointToLayer: function (feature, latlng) {
-        return L.circleMarker(L.latLng(43.16296, 13.72274), geojsonMarkerOptions);
+        return L.circleMarker(L.latLng(feature.geometry.coordinates[0], feature.geometry.coordinates[1]), geojsonMarkerOptions);
     },
     onEachFeature: onEachFeature
 }).addTo(map);
